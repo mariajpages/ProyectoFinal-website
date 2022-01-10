ab6117c713c9e657866daea122cab2670c41cd51
mariajpages <mariaj.pages@gmail.com>
2022-01-10 00:14:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000
+const app = express();
+
+// const actividadesModule = require('./utils/actividades');
+
+const viewsURL = path.join(__dirname, 'views');
+
+app.set('views', viewsURL);
+app.set('view engine', 'ejs');
+
+const publicURL = path.join(__dirname, 'public');
+
+app.use(express.static(publicURL));
+const administradorContent ="Administrador";
+const destinosContent ="Destinos";
+const resumenContent ="Resumen";
+const buenosairesContent ="Buenos Aires";
+const noroesteContent ="Noroeste";
+const litoralContent ="Litoral";
+const cuyoContent ="Cuyo";
+const cordobaContent ="Còrdoba";
+const patagoniaContent ="Patagonia";
+
+
+app.get('', (req, res) =>{
+    res.render('./pages/pagina-principal')
+
+});
+
+app.get('/administrador', (req, res) => {
+
+    res.render('./pages/administrador', {aboutStartingText: administradorContent});
+    
+});
+
+app.get('/destinos', (req, res) =>{
+    res.render('./pages/destinos', {aboutStartingText: buenosairesContent});
+})
+
+app.get('/buenosaires', (req, res) =>{
+    res.render('./pages/buenosaires', {aboutStartingText: buenosairesContent});
+})
+app.get('/cordoba', (req, res) =>{
+    res.render('./pages/cordoba', {aboutStartingText: cordobaContent});
+})
+app.get('/cuyo', (req, res) =>{
+    res.render('./pages/destinos', {aboutStartingText: cuyoContent});
+})
+app.get('/litoral', (req, res) =>{
+    res.render('./pages/litoral', {aboutStartingText: litoralContent});
+})
+app.get('/noereste', (req, res) =>{
+    res.render('./pages/destinos', {aboutStartingText: noroesteContent});
+})
+app.get('/patagonia', (req, res) =>{
+    res.render('./pages/patagonia', {aboutStartingText: patagoniaContent});
+})
+
+// // Custom Modules
+// const productsModule = require('./utils/products');
+
+// app.get('', (req, res) => {
+//     productsModule.getAllProducts((err, products) => {
+//         if (err) return res.status(500).send('Some error ocurred');
+//         if(products.length === 0) return res.status(500).send('Some Error Ocurred');
+//         console.log(products);
+//         const parsedProducts = JSON.parse(products);
+//         return res.render('pages/index', {products: parsedProducts});
+//     });
+// });
+
+// app.get('/product/:id', (req, res) => {
+//     const {id} = req.params;
+//     console.log(id);
+//     productsModule.getSingleProduct(id, (err, product) => {
+//         if (err) return res.status(404).send('Some error ocurred');
+//         const parsedProduct = JSON.parse(product);
+//         return res.render('pages/product', {product: parsedProduct});
+//     });
+// });
+
+
+
+app.listen(port, ()=>{
+    console.log(`Escuchando el puerto ${port}`)
+})
---
+++ public/js/actividades.js
@@ -0,0 +1,81 @@
+//evento clicliable modal
+// const actividadesJujuy = [
+//     {
+//         id:1,
+//         title:"Purmamarca",
+//         description:"Las casas de adobe de la ciudad y la iglesia colonial española son bonitas por derecho propio.  Es el destino de muchas personas que buscan descansar en un pueblo tranquilo y conocer los atractivos turísticos de la zona. Incluye al Cerro de los Siete Colores, Paseo de los Colorados y Paseo de los Colorados"
+//     },
+//     {
+//         id:3,
+//         title:"Tilcara",
+//         description:"Tilcara es un importante centro arqueológico debido a los vestigios de antiguos asentamientos indígenas. En ella se puede visitar Pucará de Tilcara, Jardín botánico de altura, Garganta del Diablo, Pucara de Juella y varios museos."
+//     },
+//     {
+//         id:4,
+//         title:"Humahuaca",
+//         description:"Humahuaca es un pueblo pintoresco ideal pare recorrerlo caminando por sus estrechas calles y admirar las diferentes casas y edificios. Este pueblo es integrado por el Yacimiento Arqueológico de Coctaca, el Yacimiento del Inca Cueva o Chulín e Iruya."
+//     },
+//     {
+//         id:5,
+//         title:"San Salvador de Jujuy",
+//         description:"La ciudad de San Salvador de Jujuy es la capital provincial con una rica historia y diferentes lugares que vale la pena conocer. Incluye Plaza Manuel Belgrano, Casa de Gobierno y Salón de la Bandera, Catedral San Salvador, El Cabildo (Museo Histórico Policial) y museos como el Museo Arqueológico Provincial y Museo Juan Galo Lavalle, entre otros."
+//     },
+//     {
+//         id:6,
+//         title:"La Quiaca",
+//         description:"La Quiaca es una ciudad fronteriza que por medio de un puente se llega a la ciudad boliviana de Villazón. En ella se puede visitar La Puna, Yavi, Laguna de los Pozuelos y Santa Catalina"
+//     }
+// ];
+
+// const allActividadesCard = document.querySelectorAll('.actividades-card');
+
+// allActividadesCard.forEach((el) => {
+//     el.addEventListener('click', (infoEvent) => {
+//         const id = infoEvent.target.dataset.id;
+//         const ActividadFiltrada = actividadesJujuy.filter((el) => el.id == id);
+
+//         const modalTitle = document.querySelector('.modal-title');
+//         const modalDescription = document.querySelector('.modal-description');
+        
+//         modalTitle.textContent = ActividadFiltrada[0].title;
+//         modalDescription.textContent = ActividadFiltrada[0].description;
+//     });    
+
+// });
+
+//Eventfento administrador
+
+const form = document.querySelector('form');
+
+// console.log(form)
+const url = 'http://localhost:3001/actividades'
+
+form.addEventListener('submit', (info)=>{
+    info.preventDefault()
+    console.log('Estoy funcionando')
+
+    const formData = new FormData(form)
+    const formDataJSON = Object.fromEntries(formData)
+    console.log(formDataJSON)
+
+    fetch(url,{
+        method: 'POST',
+        body: JSON.stringify(formDataJSON),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        
+    })
+    .then((res) => console.log(res) )
+    .catch((err) => console.log(err) )
+
+   
+    
+})
+
+
+
+
+
+
+// module.exports = {allActividadesCard}
