062cdd0016f42918977e2451bda902394a51b826
mariajpages <mariaj.pages@gmail.com>
2022-01-10 18:42:39 -0300

    arreglendo cafecito

--- public/js/form-comentarios.js
+++ public/js/form-comentarios.js
@@ -3,7 +3,7 @@ const formComentarios = document.querySelector('form');
 // console.log(form)
 const url = 'https://alctravel-website.herokuapp.com'
 
-formComentarios.addEventListener('submit', (info)=>{
+formComentarios.addEventListener('submit', (info) => {
     info.preventDefault()
     console.log('Estoy funcionando')
 
@@ -11,17 +11,40 @@ formComentarios.addEventListener('submit', (info)=>{
     const formDataJSON = Object.fromEntries(formData)
     console.log(formDataJSON)
 
-    fetch(url,{
+    fetch(url, {
         method: 'POST',
         body: JSON.stringify(formDataJSON),
         headers: {
             'Content-Type': 'application/json'
         }
-        
-    })
-    .then((res) => console.log(res) )
-    .catch((err) => console.log(err) )
 
-   
-    
+    })
+        .then((res) => {
+            const agregarComentarios = document.getElementById('comentarios');
+
+            for (i = 0; i < agregarComentarios; i++) {
+                item = items.childNodes[i];
+                title = item.querySelector('.slick-track .widget-title a').text;
+                subTitle = item.querySelector('.slick-track .entry-tags ul li a').text;
+                content = item.querySelector('.work-cover');
+
+                p1 = document.createElement('p');
+                p2 = document.createElement('p');
+
+                t = document.createTextNode(title);
+                b = document.createTextNode(subTitle);
+
+                p1.appendChild(t);
+                p2.appendChild(b);
+                content.appendChild(p1);
+                content.appendChild(p2);
+
+            }
+        })
+        .catch((err) => console.log(err))
+
+
+
 })
+
+agregarComentarios();
