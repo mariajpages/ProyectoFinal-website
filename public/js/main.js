4ad0da5875012ece28e748a16731334eb780c428
mariajpages <mariaj.pages@gmail.com>
2022-01-10 22:10:32 -0300

    res

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 const formComentarios = document.querySelector('form');
 
 // console.log(form)
-const url = 'https://alctravel-website.herokuapp.com/'
+const url = 'https://alctravel-website.herokuapp.com';
 
 formComentarios.addEventListener('submit', (info) => {
     info.preventDefault()
@@ -18,32 +18,32 @@ formComentarios.addEventListener('submit', (info) => {
             'Content-Type': 'application/json'
         }
 
-    })  .then(res => res.json())
-        .then((json) => {
-            const agregarComentarios = document.getElementById('comentarios');
+    })  
+        // .then((res) => {
+        //     const agregarComentarios = document.getElementById('comentarios');
 
-            for (i = 0; i < agregarComentarios.childElementCount; i++) {
-                item = agregarComentarios.childNodes[i];
-                names = item.querySelector('.card-title').text;
-                content = item.querySelector('.card-text').text;
-                Option = item.querySelector('.card-footer');
+        //     for (i = 0; i < agregarComentarios.childElementCount; i++) {
+        //         item = agregarComentarios.childNodes[i];
+        //         names = item.querySelector('.card-title').text;
+        //         content = item.querySelector('.card-text').text;
+        //         Option = item.querySelector('.card-footer');
 
-                p1 = document.createElement('p');
-                p2 = document.createElement('p');
-                p3 = document.createElement('p');
+        //         p1 = document.createElement('p');
+        //         p2 = document.createElement('p');
+        //         p3 = document.createElement('p');
 
-                n = document.createTextNode(names);
-                c = document.createTextNode(content);
-                o = document.createTextNode(Option);
+        //         n = document.createTextNode(names);
+        //         c = document.createTextNode(content);
+        //         o = document.createTextNode(Option);
 
-                p1.appendChild(n);
-                p2.appendChild(c);
-                p3.appendChild(o);
-                content.appendChild(p1);
-                content.appendChild(p2);
-                content.appendChild(p3);
-
-            }
+        //         p1.appendChild(n);
+        //         p2.appendChild(c);
+        //         p3.appendChild(o);
+        //         content.appendChild(p1);
+        //         content.appendChild(p2);
+        //         content.appendChild(p3);
+        //     }
+        console.log (res)
         })
         .catch((err) => console.log(err))
 
